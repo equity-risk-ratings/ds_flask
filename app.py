8ff8e10e9f7ed07064e9de0b9bb3afedd5b6e28d
dwightchurchill <dwight.a.churchill@gmail.com>
2019-07-30 17:45:32 -0400

    updated main app with placeholder dict

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, abort, jsonify, request, Response
+
+APP = Flask(__name__)
+
+@APP.route('/api', methods=['POST'])
+def returnRisk():
+    data = request.get_json(force=True)
+
+    risk_rating = {'amazon': "risk", 'apple': "risk", 'bofa': "risk", 'berkshire': "risk", 'google': "risk", 'jpm': "risk",
+                   'johnson': "risk", 'mastercard': "risk", 'microsoft': "risk", 'proctor': "risk", 'visa': "risk",
+                   'boeing': "risk", 'citi': "risk", 'comcast': "risk", 'cisco': "risk", 'disney': "risk",'hd': "risk",
+                   'intel': "risk", 'coke': "risk", 'mcdonalds': "risk", 'merck': "risk", 'pepsi': "risk", 'pfizer': "risk",
+                   'att': "risk", 'unh': "risk",'verizon': "risk", 'wells': "risk", 'walmart': "risk", 'boeing': "risk"}
+
+    if data['company'] in risk_rating.keys():
+        output = {'company_name': data['company'], 'risk_score': risk_rating.get(data['company'])}
+        return jsonify(output)
+    else:
+        return jsonify({'err_msg': 'We do not currently support this company.'}), 422
+
+if __name__ == '__main__':
+    APP.run(debug=True)
